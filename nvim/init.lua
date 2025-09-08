96562a2fdb1021547f31d8222981dc9164db0b46
arun.prajapati <arunkp1122@gmail.com>
2025-09-08 17:53:39 +0545

    update nvim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,7 +3,7 @@
 -- ================================
 
 -- 1. Basic Settings
-vim.opt.number = true          -- Show line numbers
+vim.opt.number = true          -- Show line numbersj
 vim.opt.relativenumber = true  -- Relative line numbers
 vim.opt.expandtab = true       -- Use spaces instead of tabs
 vim.opt.shiftwidth = 2         -- Indent by 2 spaces
@@ -69,7 +69,40 @@ require("lazy").setup({
     {
       'nvim-telescope/telescope.nvim', tag = '0.1.8',
       dependencies = { 'nvim-lua/plenary.nvim' }
-    }
+    },
+    {"nvim-treesitter/nvim-treesitter", branch = 'master', lazy = false, build = ":TSUpdate"},
+
+    { 'nvim-mini/mini.surround', version = false },
+    {
+      'jinh0/eyeliner.nvim',
+      config = function()
+        require'eyeliner'.setup {
+          -- show highlights only after keypress
+          highlight_on_key = true,
+
+          -- dim all other characters if set to true (recommended!)
+          dim = false,             
+
+          -- set the maximum number of characters eyeliner.nvim will check from
+          -- your current cursor position; this is useful if you are dealing with
+          -- large files: see https://github.com/jinh0/eyeliner.nvim/issues/41
+          max_length = 9999,
+
+          -- filetypes for which eyeliner should be disabled;
+          -- e.g., to disable on help files:
+          -- disabled_filetypes = {"help"}
+          disabled_filetypes = {},
+
+          -- buftypes for which eyeliner should be disabled
+          -- e.g., disabled_buftypes = {"nofile"}
+          disabled_buftypes = {},
+
+          -- add eyeliner to f/F/t/T keymaps;
+          -- see section on advanced configuration for more information
+          default_keymaps = true,
+        }
+      end
+    },
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
@@ -78,8 +111,66 @@ require("lazy").setup({
   checker = { enabled = true },
 })
 
+
+-- ==================== TELESCOPE PLUGIN CONFIG ===========================
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
+-- ========================================================================
+
+-- ============= TREESitter Config ============
+require'nvim-treesitter.configs'.setup {
+  -- A list of parser names, or "all" (the listed parsers MUST always be installed)
+  ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" , "javascript"},
+
+  -- Install parsers synchronously (only applied to `ensure_installed`)
+  sync_install = false,
+
+  -- Automatically install missing parsers when entering buffer
+  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+  auto_install = true,
+
+
+
+
+
+  -- List of parsers to ignore installing (or "all")
+  ignore_install = { },
+
+  ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
+  -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
+
+  highlight = {
+    enable = true,
+
+    -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
+    -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
+    -- the name of the parser)
+    -- list of language that will be disabled
+    disable = { "c", "rust" },
+    -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
+    disable = function(lang, buf)
+        local max_filesize = 100 * 1024 -- 100 KB
+        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+        if ok and stats and stats.size > max_filesize then
+            return true
+        end
+    end,
+
+    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+    -- Instead of true it can also be a list of languages
+    additional_vim_regex_highlighting = false,
+  },
+}
+-- ===========================================
+
+
+
+--  ============= mini.surround ====================
+require('mini.surround').setup()
+-- =================================================
